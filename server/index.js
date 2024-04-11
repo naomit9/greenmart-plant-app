07fe11a128aa942e729c861ea67ddfd03d338b94
nhutr <nhutr@LAPTOP-EJRNGLNE>
2024-04-11 13:47:49 -0400

    update upload path in server

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors(
 ));
 app.use(express.urlencoded( { extended: true } ))
 app.use(express.json());
-app.use("/uploads", express.static("uploads"))
+app.use(express.static("uploads"))
 
 app.get("/api/plants", async (req, res) => {
     try {
